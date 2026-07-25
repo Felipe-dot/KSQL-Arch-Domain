fcf98ec442fd8d53e1ab86e1d31e212edefdf0b5
Felipe-dot <felipeluis610@gmail.com>
2026-07-25 17:04:09 -0300

    :construction: Improve infrastructure

--- domain/src/main/java/com/domain/order/Order.java
+++ domain/src/main/java/com/domain/order/Order.java
@@ -1,17 +1,22 @@
 package com.domain.order;
 
 import com.domain.Aggregation;
+import com.domain.exceptions.DomainException;
+import com.domain.validators.Error;
 import com.domain.validators.ValidationHandlerDomain;
 
-import javax.xml.validation.ValidatorHandler;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
+import java.util.Random;
 
 public class Order extends Aggregation<OrderID> implements Cloneable {
 
     private BigDecimal orderValue;
     private String channel;
     private String paymentStatus;
+    private String orderStatus;
+    private String reason;
     private Instant created;
     private Instant updated;
 
@@ -20,6 +25,8 @@ public class Order extends Aggregation<OrderID> implements Cloneable {
         this.orderValue = orderValue;
         this.channel = channel;
         this.paymentStatus = paymentStatus;
+        this.orderStatus = "PAYMENT_PENDING";
+        this.reason = "NO REASON - OK";
         this.created = created;
         this.updated = updated;
     }
@@ -27,7 +34,11 @@ public class Order extends Aggregation<OrderID> implements Cloneable {
     public static Order newOrder(BigDecimal orderValue, String channel, String paymentStatus) {
         final var id = OrderID.uniqueID();
         final var now = Instant.now();
-        return new Order(id,orderValue, channel, paymentStatus, now, now);
+        return new Order(id, orderValue, channel, paymentStatus, now, now);
+    }
+
+    public static Order aggregate(OrderID id, BigDecimal orderValue, String channel, String paymentStatus, Instant created, Instant updated) {
+        return new Order(id, orderValue, channel, paymentStatus, created, updated);
     }
 
     public Order update(String orderId, BigDecimal orderValue, String channel, String paymentStatus) {
@@ -38,9 +49,104 @@ public class Order extends Aggregation<OrderID> implements Cloneable {
         return this;
     }
 
+    public Order check(String orderId, BigDecimal orderValue, String channel, String paymentStatus) {
+        this.orderValue = orderValue;
+        this.channel = channel;
+        this.paymentStatus = paymentStatus;
+        this.orderStatus = "PAYMENT_PENDING";
+        this.reason = "NO REASON - OK";
+        this.updated = Instant.now();
+        applyDecisionRulesForOrderPaymentStatus();
+        return this;
+    }
+
+
+    public enum CancellationReason {
+        SEPARATION_PROBLEM("SEPARATION PROBLEM"),
+        RUPTURE("NO STOCK"),
+        INSUFFICIENT_BALANCE("INSUFFICIENT BALANCE");
+
+        private final String description;
+
+        CancellationReason(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    private void evaluateOrderCancelReason(Random random) {
+        CancellationReason[] reasons = CancellationReason.values();
+        if (random.nextInt(100) < 20) {
+            CancellationReason reason = reasons[random.nextInt(reasons.length)];
+            cancelOrder(reason);
+        }
+    }
+
+    private void cancelOrder(CancellationReason reason) {
+        this.orderStatus = "CANCELLED";
+        this.reason = reason.getDescription();
+        throw new DomainException("Order Cancelled", List.of(new Error("Order Cancelled due to: " + reason.getDescription())));
+    }
+
+    private void applyDecisionRulesForOrderPaymentStatus() {
+        Random random = new Random();
+
+        if ("SITE".equals(this.channel) || "APP".equals(this.channel)) {
+            switch (this.paymentStatus.toUpperCase()) {
+                case "FRAUD_CHECK":
+                    if (random.nextBoolean()) {
+                        this.orderStatus = "PAYMENT_PENDING";
+                    } else {
+                        if (random.nextInt(100) < 30) {
+                            this.orderStatus = "CANCELLED";
+                            cancelOrder(CancellationReason.RUPTURE);
+                        }
+                    }
+                    break;
+                case "AUTHORIZED":
+                    int decision = random.nextInt(3);
+                    if (decision == 0) {
+                        this.orderStatus = "FRAUD_CHECK_MANUAL";
+                        throw new DomainException("Order Check Manual Fraud ",
+                                List.of(new Error("Order contains problems to be checked automatically")));
+                    } else if (decision == 1) {
+                        this.orderStatus = "PAID";
+                    } else {
+                        evaluateOrderCancelReason(random);
+                    }
+                    break;
+                default:
+                    this.orderStatus = "PAYMENT_PENDING";
+            }
+
+            if (this.orderValue.compareTo(new BigDecimal("10000")) > 0) {
+                if (random.nextInt(100) < 70) {
+                    this.orderStatus = "PAID";
+                } else {
+                    if (random.nextInt(100) < 20) {
+                        cancelOrder(CancellationReason.INSUFFICIENT_BALANCE);
+                    }
+                }
+            } else {
+                if (random.nextInt(100) < 30) {
+                    this.orderStatus = "PAID";
+                } else {
+                    if (random.nextInt(100) < 10) {
+                        cancelOrder(CancellationReason.SEPARATION_PROBLEM);
+                    }
+                }
+            }
+        } else {
+            this.orderStatus = "PAID";
+        }
+    }
+
     @Override
     public void validate(ValidationHandlerDomain handler) {
-         new OrderValidator( this, handler).validate();
+        new OrderValidator(this, handler).validate();
     }
 
     public String getChannel() {
@@ -63,10 +169,14 @@ public class Order extends Aggregation<OrderID> implements Cloneable {
         return updated;
     }
 
-    @Override
-    public OrderID getId() {
-        return super.getId();
+    public String getOrderStatus() {
+        return orderStatus;
     }
+
+    public String getReason() {
+        return reason;
+    }
+
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
--- infrastructure/src/main/java/com/infrastructure/InfraStructureApplication.java
+++ infrastructure/src/main/java/com/infrastructure/InfraStructureApplication.java
@@ -1,13 +1,21 @@
 package com.infrastructure;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @ComponentScan(value = "com.infrastructure")
+@EnableJpaRepositories
+@EnableRabbit
+@EnableKafka
 public class InfraStructureApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(InfraStructureApplication.class);
     }
+
 }
